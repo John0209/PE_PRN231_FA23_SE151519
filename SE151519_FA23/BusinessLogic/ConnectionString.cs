c733f655a01b6ea265328ed9e63b9db7b0fcfd23
Tuấn Vũ <113121915+John0209@users.noreply.github.com>
2023-11-08 09:50:12 +0700

    update

---
+++ SE151519_FA23/BusinessLogic/ConnectionString.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    internal class ConnectionString
+    {
+        //dotnet ef dbcontext scaffold "Server=DESKTOP-0MQNV7A;Uid=sa;Pwd=12345;Database=PRN231_SU23_StudentGroupDB;TrustServerCertificate=True;" Microsoft.EntityFrameworkCore.SqlServer --output-dir Models
+
+    }
+}
